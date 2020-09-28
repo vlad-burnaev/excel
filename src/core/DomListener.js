d1beb199a7c7385c9dd94d57ea78bf533939b636
Vlad Burnaev <vlad_burn@icloud.com>
2020-09-29 01:59:33 +0300

    Finish base framework functional

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -16,11 +16,16 @@ export class DomListener {
         const name = this.name
         throw new Error(`Method ${method} is not implemented in ${name}`)
       }
-      this.$root.on(listener, this[method].bind(this))
+      this[method] = this[method].bind(this)
+      this.$root.on(listener, this[method])
     })
   }
 
   removeDomListeners() {
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener)
+      this.$root.off(listener, this[method])
+    })
   }
 }
 
